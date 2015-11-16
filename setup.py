023a0461f8c5d396e4cc638c8fba138c80b3f1dd
Christopher Riley <riguy724@gmail.com>
2015-11-16 14:00:14 -0500

    Fix naming conflict with import statement

--- setup.py
+++ setup.py
@@ -2,23 +2,26 @@
 """
     Pip module setup
 """
-from setuptools import setup
+from setuptools import setup, find_packages
 
 INSTALL_REQUIRES = [
-    'click >= 0.5.0'
+    'Click'
 ]
 
-setup(name='elasticsearch-import',
-      version='0.0.0',
-      description='Elasticsearch data import',
-      url='http://github.com/riguy724/elasticsearch-import',
-      author='Christopher Riley',
-      author_email='riguy724@gmail.com',
-      license='MIT',
-      install_requires=INSTALL_REQUIRES,
-      packages=["import", "import/cli"],
-      entry_points={
-        'console_scripts': [
-            'elastic-import=import.cli.main:hello',
-        ],
-      })
+setup(
+    name='elastic-import',
+    version='0.0.3',
+    description='Elasticsearch data import',
+    url='http://github.com/riguy724/elasticsearch-import',
+    author='Christopher Riley',
+    author_email='riguy724@gmail.com',
+    license='MIT',
+    install_requires=INSTALL_REQUIRES,
+    include_package_data=True,
+    packages=find_packages(),
+    entry_points={
+      'console_scripts': [
+          'elastic-import = esimport.cli.main:hello',
+      ],
+    },
+)
